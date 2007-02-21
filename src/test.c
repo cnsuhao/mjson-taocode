64fcce634836a87a65d913533e54a720fa06c407
rui_maciel <rui_maciel@4b291280-012a-0410-bf21-f991acb00d4c>
2007-02-21 19:46:30 +0000

    initial import

---
+++ src/test.c
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Rui Maciel   *
+ *   rui.maciel@gmail.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "json.h"
+
+
+int main ( )
+{
+// 	char text[] = "{\"id\":1215,\"value\":13}";
+// 	char text[] = "{\"id\":[[\"fir\",\"sec\"],\"second\"],\"value\":13}";
+// 	char text[] = "{\"id\":\"file\",\"value\":\"File\"}";
+// 	char text[] = "{\"label1\":{\"label2\":\"value1\"},\"label1\":{\"label3\":\"value2\", \"label2\":\"value1\"},\"label3\":\"value2\",\"label4\":\"value3\"}";
+// 	char text[] = "{\"label1\":false, \"label2\":false}";
+// 	char text[] = "{\"label\":\"false\", \"label\":\"false\"}";
+// 	char text[] = "{\"menu\": { \"id\": \"file\", \"value\": \"File\", \"popup\": { \"menuitem\": [ {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}, {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"}, {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"} ] } }}";
+// 	char text[] = "{\"widget\": { \"debug\": \"on\", \"window\": { \"title\": \"Sample Konfabulator Widget\", \"name\": \"main_window\", \"width\": 500, \"height\": 500 }, \"image\": { \"src\": \"Images/Sun.png\", \"name\": \"sun1\", \"hOffset\": 250, \"vOffset\": 250, \"alignment\": \"center\" }, \"text\": { \"data\": \"Click Here\", \"size\": 36, \"style\": \"bold\", \"name\": \"text1\", \"hOffset\": 250, \"vOffset\": 100, \"alignment\": \"center\", \"onMouseUp\": \"sun1.opacity = (sun1.opacity / 100) * 90;\" } }}";
+	char text[] = "-0.1";
+
+
+	struct json_value *root = NULL;
+
+	printf("passing text to tree:\n");
+	root = json_string_to_tree(text);
+
+	if(root)
+	{
+		printf("passing tree to text:\n");
+		char *foo = json_tree_to_string(root);
+		printf("%s\n",foo);
+	// 	json_free_value(root);
+	}
+	
+	return EXIT_SUCCESS;
+}
