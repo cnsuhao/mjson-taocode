3db8c0761b39e28b9dc3a26dcba5001fbab4a650
rui_maciel <rui_maciel@4b291280-012a-0410-bf21-f991acb00d4c>
2008-05-27 15:12:18 +0000

    reverted test code which didn't belong in the previous commit

--- trunk/src/test.c
+++ trunk/src/test.c
@@ -5,63 +5,56 @@
 #include "json.h"
 #include "json_helper.h"
 
-enum json_error json_tree_to_file (json_t * root, FILE *file)
-{
-}
-
-
-
 #define BUFFER_SIZE 1024
-
 int
 main (void)
 {
-	char buffer[BUFFER_SIZE];
-	char *temp = NULL;
-	unsigned int error = JSON_INCOMPLETE_DOCUMENT;
+  char buffer[BUFFER_SIZE];
+  char *temp = NULL;
+  unsigned int error = JSON_INCOMPLETE_DOCUMENT;
 
-	struct json_parsing_info state;
+  struct json_parsing_info state;
 
-	json_jpi_init(&state);
+  json_jpi_init (&state);
 
-	while  ( (error == JSON_WAITING_FOR_EOF) || (error == JSON_INCOMPLETE_DOCUMENT) )
+  while ((error == JSON_WAITING_FOR_EOF)
+	 || (error == JSON_INCOMPLETE_DOCUMENT))
+    {
+      if (fgets (buffer, BUFFER_SIZE, stdin) != NULL)
 	{
-		if(fgets (buffer, BUFFER_SIZE, stdin) != NULL)
-		{
-			switch(error = json_parse_fragment ( &state, buffer))
-			{
-				case JSON_OK:
-					printf("complete\n");
-					json_tree_to_string(state.cursor, &temp);
-					printf("%s\n",temp);
-					break;
+	  switch (error = json_parse_fragment (&state, buffer))
+	    {
+	    case JSON_OK:
+	      printf ("complete\n");
+	      json_tree_to_string (state.cursor, &temp);
+	      printf ("%s\n", temp);
+	      break;
 
-				case JSON_WAITING_FOR_EOF:
-				case JSON_INCOMPLETE_DOCUMENT:
-					break;
+	    case JSON_WAITING_FOR_EOF:
+	    case JSON_INCOMPLETE_DOCUMENT:
+	      break;
 
-				default:
-					printf("Some error occurred: %d\n", error);
-			}
-		}
-		else
-		{
-			if(error == JSON_WAITING_FOR_EOF)
-				error = JSON_OK;
-			else
-				error = JSON_UNKNOWN_PROBLEM;
-		}
+	    default:
+	      printf ("Some error occurred: %d\n", error);
+	    }
 	}
-	if(error == JSON_OK)
+      else
 	{
-		printf("printing...\n");
-		json_tree_to_file(state.cursor, stdout);
+	  if (error == JSON_WAITING_FOR_EOF)
+	    error = JSON_OK;
+	  else
+	    error = JSON_UNKNOWN_PROBLEM;
 	}
-	else
-	{
-		printf("Document wasn't valid.\n");
-	}
-	/* perform cleanup */
-	json_free_value(&state.cursor);
-	return 0;
+    }
+  if (error == JSON_OK)
+    {
+      json_render_tree (state.cursor);
+    }
+  else
+    {
+      printf ("Document wasn't valid.\n");
+    }
+  /* perform cleanup */
+  json_free_value (&state.cursor);
+  return 0;
 }
