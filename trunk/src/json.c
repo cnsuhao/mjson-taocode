c179145e675494326dd5735964b1c3ad76fa4e1e
rui_maciel <rui_maciel@4b291280-012a-0410-bf21-f991acb00d4c>
2009-03-10 18:48:00 +0000

    unescaped control code present in a string means illegal character

--- trunk/src/json.c
+++ trunk/src/json.c
@@ -1061,6 +1061,41 @@ lexer (char *buffer, char **p, unsigned int *state, rcstring ** text)
 				assert (*text != NULL);
 				switch (**p)
 				{
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+				case 10:	/* line feed */
+				case 11:
+				case 12:
+				case 13:	/* carriage return */
+				case 14:
+				case 15:
+				case 16:
+				case 17:
+				case 18:
+				case 19:
+				case 20:
+				case 21:
+				case 22:
+				case 23:
+				case 24:
+				case 25:
+				case 26:
+				case 27:
+				case 28:
+				case 29:
+				case 30:
+				case 31:
+					/* ASCII control characters can only be present in a JSON string if they are escaped. If not then the document is invalid */
+					return JSON_ILLEGAL_CHARACTER;
+					break;
+
 				case '\"':	/* close JSON string */
 					/* it is expected that, in the routine that calls this function, text is set to NULL */
 					*state = 0;
