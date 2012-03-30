92c92520fde8096d398e5c9d9525a29981c66de7
rui_maciel <rui_maciel@4b291280-012a-0410-bf21-f991acb00d4c>
2012-03-30 09:30:59 +0000

    Added fixes for some compiler warnings.  Big thanks to Daniel J Blueman

--- src/json.c
+++ src/json.c
@@ -2805,14 +2805,9 @@ json_parse_document (json_t ** root, char *text)
 enum json_error
 json_saxy_parse (struct json_saxy_parser_status *jsps, struct json_saxy_functions *jsf, char c)
 {
-	/*TODO handle a string instead of a single char */
-	/* temp variables */
-	rcstring *temp;
-
 	/* make sure everything is in it's place */
 	assert (jsps != NULL);
 	assert (jsf != NULL);
-	temp = NULL;
 
 	/* goto where we left off */
 	switch (jsps->state)
